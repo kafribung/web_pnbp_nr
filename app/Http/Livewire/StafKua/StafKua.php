39232faab05d6b9e8eba2c3d3d43deb12095445c
Kafriansyah <kafri@prevalensi.com>
2021-07-20 23:56:51 +0800

    paginate

--- app/Http/Livewire/StafKua/StafKua.php
+++ app/Http/Livewire/StafKua/StafKua.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire\StafKua;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class StafKua extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.staf-kua.staf-kua');
+        // $stafKuas = User::with('roles')->role(['kalukku', 'sampaga'])->get();
+        // $stafKuas = User::with('roles')->paginate(1);
+        $stafKuas = User::whereHas('roles', function($query) {
+            $query->where('name','!=', 'admin');
+        })->paginate(10);
+        return view('livewire.staf-kua.staf-kua', compact('stafKuas'));
+    }
+
+    public function paginationView()
+    {
+        return 'vendor.livewire.costume';
     }
 }
---
+++ resources/views/vendor/livewire/costume.blade.php
@@ -0,0 +1,103 @@
+<div>
+    @if ($paginator->hasPages())
+    <div
+        class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+        <span class="flex items-center col-span-3">
+            Menunjukan  {{ $paginator->firstItem() }}-{{ $paginator->lastItem() }} dari {{ $paginator->total() }}
+        </span>
+        <span class="col-span-2"></span>
+        <!-- Pagination -->
+        <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+            <nav aria-label="Table navigation">
+                <ul class="inline-flex items-center">
+                    <li>
+                        @if (!$paginator->onFirstPage())
+                        <button wire:click="previousPage" wire:loading.attr="disabled" dusk="previousPage.before"
+                            class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
+                            aria-label="Previous">
+                            <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                <path
+                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                    clip-rule="evenodd" fill-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                        @endif
+                    </li>
+                    <li>
+                        @if ($paginator->hasMorePages())
+                        <button wire:click="nextPage" wire:loading.attr="disabled" dusk="nextPage.before"
+                            class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
+                            aria-label="Previous">
+                            <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                <path
+                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                    clip-rule="evenodd" fill-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                        @endif
+                    </li>
+
+                    {{-- Pagination Elements --}}
+                    @foreach ($elements as $element)
+                    {{-- "Three Dots" Separator --}}
+                    @if (is_string($element))
+                    <span aria-disabled="true">
+                        <span
+                            class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 cursor-default leading-5">{{ $element }}</span>
+                    </span>
+                    @endif
+
+                    {{-- Array Of Links --}}
+                    @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                    <span wire:key="paginator-page{{ $page }}">
+                        @if ($page == $paginator->currentPage())
+                        <span aria-current="page">
+                            <span
+                                class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">{{ $page }}</span>
+                        </span>
+                        {{-- <button
+                        class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">
+                        {{ $page }}
+                        </button> --}}
+                        @else
+                        <button wire:click="gotoPage({{ $page }})" class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                            {{ $page }}
+                        </button>
+                        {{-- <button wire:click="gotoPage({{ $page }})"
+                            class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                            aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                            {{ $page }}
+                        </button> --}}
+                        @endif
+                    </span>
+                    @endforeach
+                    @endif
+                    @endforeach
+                    <li>
+                        {{-- Next Page Link --}}
+                        @if ($paginator->hasMorePages())
+                        <button wire:click="nextPage" dusk="nextPage.after" rel="next"
+                            class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple"
+                            aria-label="Next">
+                            <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                <path
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    clip-rule="evenodd" fill-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                        @else
+                        <span aria-disabled="true" aria-label="{{ __('pagination.next') }}">
+                            <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                <path
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    clip-rule="evenodd" fill-rule="evenodd"></path>
+                            </svg>
+                        </span>
+                        @endif
+                    </li>
+                </ul>
+            </nav>
+        </span>
+    </div>
+    @endif
