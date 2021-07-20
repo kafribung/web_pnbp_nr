155cdc0e4ff58ee7652368c4a760d9b35ec0a989
Kafriansyah <kafri@prevalensi.com>
2021-07-20 16:24:23 +0800

    sidebar

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -24,12 +24,12 @@
         </ul>
         <ul>
             <li class="relative px-6 py-3">
-                @if (request()->routeIs('dashboard'))
+                @if (request()->routeIs('staf-kua'))
                 <span class="absolute inset-y-0 left-0 w-1 bg-green-600 rounded-tr-lg rounded-br-lg"
                     aria-hidden="true"></span>
                 @endif
                 <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                    href="forms.html">
+                    href="{{ route('staf-kua') }}">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
                         stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                         <path
