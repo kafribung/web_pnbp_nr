f4e1be75d48327d7378d471ed03a07d990a8478a
Kafriansyah <kafri@prevalensi.com>
2021-10-16 00:31:09 +0800

    jasa profesi

--- app/Http/Livewire/JasaProfesiDanTransport/JasaProfesiDanTransport.php
+++ app/Http/Livewire/JasaProfesiDanTransport/JasaProfesiDanTransport.php
@@ -4,13 +4,20 @@ namespace App\Http\Livewire\JasaProfesiDanTransport;
 
 use App\Models\Penghulu;
 use App\Models\Pernikahan;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class JasaProfesiDanTransport extends Component
 {
     public function render()
     {
-        $penghulus = Penghulu::with('pernikahans', 'golongan')->where('kua_id', auth()->user()->kua_id)->get();
+        $penghulus = Penghulu::with('pernikahans', 'golongan')
+                    ->whereHas('pernikahans', function($query){
+                        $query->whereMonth('date_time', Carbon::now()->month)
+                                ->whereYear('date_time', Carbon::now()->year);
+                    })
+                    ->where('kua_id', auth()->user()->kua_id)
+                    ->get();
 
         return view('livewire.jasa-profesi-dan-transport.jasa-profesi-dan-transport', compact('penghulus'));
     }
