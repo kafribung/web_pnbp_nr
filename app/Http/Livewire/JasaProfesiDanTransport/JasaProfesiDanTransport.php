8a937bd4463533579811b15d72fb1c894242acff
Kafriansyah <kafri@prevalensi.com>
2021-10-15 18:47:17 +0800

    jasa profesi dan transport

--- app/Http/Livewire/JasaProfesiDanTransport/JasaProfesiDanTransport.php
+++ app/Http/Livewire/JasaProfesiDanTransport/JasaProfesiDanTransport.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire\JasaProfesiDanTransport;
 
+use App\Models\Penghulu;
+use App\Models\Pernikahan;
 use Livewire\Component;
 
 class JasaProfesiDanTransport extends Component
 {
     public function render()
     {
-        return view('livewire.jasa-profesi-dan-transport.jasa-profesi-dan-transport');
+        $penghulus = Penghulu::with('pernikahans', 'golongan')->where('kua_id', auth()->user()->kua_id)->get();
+
+        return view('livewire.jasa-profesi-dan-transport.jasa-profesi-dan-transport', compact('penghulus'));
     }
 }
--- resources/views/livewire/jasa-profesi-dan-transport/jasa-profesi-dan-transport.blade.php
+++ resources/views/livewire/jasa-profesi-dan-transport/jasa-profesi-dan-transport.blade.php
@@ -20,15 +20,16 @@
                 </div>
             </div>
 
-            <table class="w-full whitespace-no-wrap">
+            <table class="w-12">
                 <thead>
                     <tr
                         class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                         <th class="px-4 py-3" rowspan="2">No</th>
-                        <th class="px-4 py-3" rowspan="2">Nama Pelaksana</th>
+                        <th class="px-4 py-3" rowspan="2">Penghulu</th>
                         <th class="px-4 py-3" rowspan="2">Pangkat/Gol</th>
                         <th class="px-4 py-3" rowspan="2">Jml NR</th>
                         <th class="px-4 py-3" rowspan="2">Satuan PNBP</th>
+                        <th class="px-4 py-3" rowspan="2">Jumlah PNBP</th>
                         <th class="px-4 py-3 text-center" colspan="2">Transport</th>
                         <th class="px-4 py-3 text-center" colspan="4">Jasa Profesi</th>
                         <th class="px-4 py-3" rowspan="2">Jumlah Permohonan Pembayaran</th>
@@ -45,12 +46,18 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
-                    {{-- @forelse ($pernikahans as $index => $pernikahan)
+                    @forelse ($penghulus as $index => $penghulu)
                     <tr class="text-gray-700 dark:text-gray-400">
                         <td class="px-4 py-3 text-sm">
-                            {{ (($pernikahans->currentPage() - 1 ) * $pernikahans->perPage() ) + $loop->iteration }}
+                            {{ $index+1 }}
                         </td>
-                        <td class="px-4 py-3">
+                        <td class="px-4 py-3 text-sm"> {{ $penghulu->name }} </td>
+                        <td class="px-4 py-3 text-sm"> {{ $penghulu->golongan->name }} </td>
+                        <td class="px-4 py-3 text-sm"> {{ $jumlahNR = $penghulu->pernikahans->count() }} </td>
+                        <td class="px-4 py-3 text-sm"> {{ number_format($satuanPnbpNr = 600000, 2)  }} </td>
+                        <td class="px-4 py-3 text-sm"> {{ number_format($satuanPnbpNr * $jumlahNR) }} </td>
+
+                        {{-- <td class="px-4 py-3">
                             <div class="flex items-center text-sm">
                                 <div>
                                     <p class="font-semibold">{{ $pernikahan->male }} Bin {{ $pernikahan->male_father }}</p>
@@ -65,28 +72,19 @@
                                     <p class="text-xs text-gray-600 dark:text-gray-400">{{ $pernikahan->female_age }} tahun</p>
                                 </div>
                             </div>
-                        </td>
-                        <td class="px-4 py-3 text-sm"> {{ $pernikahan->village }} </td>
-                        <td class="px-4 py-3 text-sm"> {{ $pernikahan->marriage_certificate_number }} </td>
-                        <td class="px-4 py-3 text-sm"> {{ $pernikahan->perforation_number }} </td>
-                        <td class="px-4 py-3 text-sm"> {{ $pernikahan->penghulu->name ?? null }} </td>
-                        <td class="px-4 py-3 text-sm"> {{ Carbon\Carbon::parse($pernikahan->date_time)->isoFormat('dddd')  }} </td>
-                        <td class="px-4 py-3 text-sm"> {{  date('d M Y', strtotime($pernikahan->date_time)) }} </td>
-                        <td class="px-4 py-3 text-sm"> {{ $pernikahan->peristiwa_nikah->name ?? null }} </td>
-
+                        </td> --}}
                         <td class="px-4 py-3">
                             <div class="flex items-center space-x-4 text-sm">
-                                <x-button-edit-delete metode='edit' wire:click="$emitTo('pernikahan.form', 'edit', {{ $pernikahan->id }})" class="hover:text-yellow-700 text-yellow-600 focus:shadow-outline-yellow"></x-button-edit-delete>
-                                <x-button-edit-delete metode='delete' wire:click="$emitTo('pernikahan.form', 'delete', {{ $pernikahan->id }})" class="hover:text-red-700 text-red-600 focus:shadow-outline-red"></x-button-edit-delete>
+                                <x-button-edit-delete metode='edit' wire:click="$emitTo('pernikahan.form', 'edit', {{ $penghulu->id }})" class="hover:text-yellow-700 text-yellow-600 focus:shadow-outline-yellow"></x-button-edit-delete>
+                                <x-button-edit-delete metode='delete' wire:click="$emitTo('pernikahan.form', 'delete', {{ $penghulu->id }})" class="hover:text-red-700 text-red-600 focus:shadow-outline-red"></x-button-edit-delete>
                             </div>
                         </td>
                     </tr>
                     @empty
                         <td colspan="10" class="items-center text-center">Data tidak ditemukan !</td>
-                    @endforelse --}}
+                    @endforelse
                 </tbody>
             </table>
         </div>
-        {{-- {{ $pernikahans->links() }} --}}
     </div>
 </div>
