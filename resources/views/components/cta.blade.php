fdf721b35bd7f8ddeeb3432f2448c527933da29c
kafribung <60900117001@uin-alauddin.ac.id>
2021-11-18 18:38:58 +0800

    cta

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -1,6 +1,6 @@
 <!-- CTA -->
 <div
-    class="flex items-center p-4 mb-4 text-sm font-semibold text-green-100 bg-green-600 rounded-tl-3xl rounded-br-xl shadow-lg focus:outline-none focus:shadow-outline-green">
+    class="flex items-center p-4 mb-4 text-sm font-semibold text-green-100 bg-green-600 rounded-lg shadow-lg focus:outline-none focus:shadow-outline-green">
     <div class="flex items-center">
         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
             <path
