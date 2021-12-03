f0798f2912be5c9c771fc9e1d32b8f7106bca63c
kafribung <60900117001@uin-alauddin.ac.id>
2021-12-03 18:50:29 +0800

    dashboard

--- resources/views/livewire/dashboard/dashboard.blade.php
+++ resources/views/livewire/dashboard/dashboard.blade.php
@@ -154,7 +154,7 @@
                             <th class="px-4 py-3 text-center" colspan="4">Bebas Biaya</th>
                             <th class="px-4 py-3" rowspan="3">Jml NR</th>
                             <th class="px-4 py-3" rowspan="3">Total PNBP</th>
-                            <th class="px-4 py-3 text-center" colspan="3">Berdasarkan Usia</th>
+                            <th class="px-4 py-3 text-center" colspan="6">Berdasarkan Usia</th>
                         </tr>
                         <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                             <th class="px-4 py-3" rowspan="2">Kantor</th>
