3d26fb26e254543b5aa6b518ab138e8ce375a038
kafribung <60900117001@uin-alauddin.ac.id>
2021-12-04 16:08:48 +0800

    pernikahan

--- app/Http/Livewire/Dashboard/Dashboard.php
+++ app/Http/Livewire/Dashboard/Dashboard.php
@@ -22,14 +22,26 @@ class Dashboard extends Component
 
     public function render()
     {
-        
-        $pernikahans = Pernikahan::
-                        whereMonth('date_time', $this->currnetMonth)
+
+        $pernikahans = Pernikahan::with('desa')
+                        ->whereMonth('date_time', $this->currnetMonth)
                         ->whereYear('date_time', $this->currnetYear)
                         ->where('kua_id', auth()->user()->kua_id)
-                        ->get()
-                        ->groupBy('village')
-                        ->all();
+                        ->whereHas('desa', function($query){
+                            $query->groupBy('name');
+                        })
+                        ->get();
+
+        // $pernikahans = Db::table('pernikahans')->selectRaw('count(pernikahans.id) as count, desas.name')
+        //                 ->join('desas', 'pernikahans.desa_id', 'desas.id')
+        //                 ->groupBy('desas.name')
+        //                 ->whereMonth('date_time', $this->currnetMonth)
+        //                 ->whereYear('date_time', $this->currnetYear)
+        //                 ->where('pernikahans.kua_id', auth()->user()->kua_id)
+        //                 ->where()
+        //                 ->get();
+
+
 
         // dd($pernikahans);
 
