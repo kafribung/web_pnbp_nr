9dd98e25911cab3c2021c664151c1507b9b1258b
kafribung <60900117001@uin-alauddin.ac.id>
2021-12-25 20:26:21 +0800

    jumlah jasa profesi

--- resources/views/livewire/jasa-profesi-dan-transport/jasa-profesi-dan-transport.blade.php
+++ resources/views/livewire/jasa-profesi-dan-transport/jasa-profesi-dan-transport.blade.php
@@ -62,7 +62,7 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
-                    <tr class="text-center text-xs font-bold">
+                    <tr class="text-center text-xs font-semibold">
                         <td>a</td>
                         <td>b</td>
                         <td>c</td>
@@ -77,10 +77,23 @@
                         <td>l=j-k</td>
                         <td>m=h+l</td>
                     </tr>
-                    @forelse ($penghulus as $index => $penghulu)
+                    {{-- Set array variable --}}
                     @php
-                            $totaljumlahNr = [];
+                        $totJumlahNR      = [];
+                        $totSatuanPnbpNr  = [];
+                        $totJumlahPNBP    = [];
+
+                        $totJasaTransport = [];
+                        $totJumTransport  = [];
+
+                        $totJasPro        = [];
+                        $totJumJasaProfesi= [];
+                        $totPPH           = [];
+                        $totJumPengPPH    = [];
+                        $totJumPerPem     = [];
                     @endphp
+                    @forelse ($penghulus as $index => $penghulu)
+
                     <tr class="text-gray-700 dark:text-gray-400">
                         <td class="px-4 py-3 text-xs text-center">
                             {{ $index+1 }}
@@ -89,39 +102,54 @@
                         <td class="px-4 py-3 text-xs"> {{ $penghulu->golongan->name }} </td>
                         <td class="px-4 py-3 text-xs text-center"> {{$jumlahNR = $penghulu->pernikahans()->whereMonth('date_time', $currnetMonth)->whereYear('date_time', $currnetYear)->whereHas('peristiwa_nikah', fn($query) => $query->where('name', 'Luar Balai Nikah'))->count() }} </td>
                         <td class="px-4 py-3 text-xs text-center"> {{ number_format($satuanPnbpNr = 600000, 2)  }} </td>
-                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($satuanPnbpNr * $jumlahNR, 2) }} </td>
+                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jumlahPNBP   = $satuanPnbpNr * $jumlahNR, 2) }} </td>
 
                         {{-- Transport --}}
                         @if (auth()->user()->kua->name == 'Tommo' || auth()->user()->kua->name == 'Tapalang Barat' || auth()->user()->kua->name == 'Bonehau' || auth()->user()->kua->name == 'Kalumpang' || auth()->user()->kua->name == 'Kepulauan Balabalakang')
-                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jasaTransport = $penghulu->pernikahans()->sum('transport'), 2) }} </td>
+                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jasaTransport= $penghulu->pernikahans()->sum('transport'), 2) }} </td>
                         @else
                         {{-- Jika KUa tipologi C --}}
-                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jasaTransport = 100000, 2) }} </td>
+                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jasaTransport= 100000, 2) }} </td>
                         @endif
                         <td class="px-4 py-3 text-xs text-center"> {{ number_format($jumTransport = $jumlahNR * $jasaTransport, 2) }} </td>
 
                         {{-- Jasa Profesi --}}
-                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jasPro = $penghulu->jasa_profesi, 2) }} </td>
+                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jasPro         = $penghulu->jasa_profesi, 2) }} </td>
                         <td class="px-4 py-3 text-xs text-center"> {{ number_format($jumJasaProfesi = $jasPro * $jumlahNR, 2) }} </td>
-                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($pph = $penghulu->pph($jumJasaProfesi), 2) }} </td>
+                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($pph        = $penghulu->pph($jumJasaProfesi), 2) }} </td>
                         <td class="px-4 py-3 text-xs text-center"> {{ number_format($jumPengPPH = $jumJasaProfesi - $pph, 2) }} </td>
-                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jumTransport + $jumPengPPH, 2) }} </td>
+                        <td class="px-4 py-3 text-xs text-center"> {{ number_format($jumPerPem  = $jumTransport + $jumPengPPH, 2) }} </td>
+
+                        @php
+                            $totJumlahNR[]        .= $jumlahNR;
+                            $totSatuanPnbpNr[]    .= $satuanPnbpNr;
+                            $totJumlahPNBP[]      .= $jumlahPNBP;
+
+                            $totJasaTransport[]   .= $jasaTransport;
+                            $totJumTransport[]    .= $jumTransport;
+
+                            $totJasPro[]          .= $jasPro;
+                            $totJumJasaProfesi[]  .= $jumJasaProfesi;
+                            $totPPH[]             .= $pph;
+                            $totJumPengPPH[]      .= $jumPengPPH;
+                            $totJumPerPem[]          .= $jumPerPem;
+                        @endphp
                     </tr>
                     @empty
                         <td colspan="20" class="items-center text-center">Data tidak ditemukan !</td>
                     @endforelse
-                    <tr class="text-center text-xs font-extrabold">
+                    <tr class="text-center text-xs font-bold">
                         <td colspan="3">Jumlah</td>
-                        <td>{{ 'sa' }}</td>
-                        <td>c</td>
-                        <td>d</td>
-                        <td>e</td>
-                        <td>f=d*e</td>
-                        <td>g</td>
-                        <td>h=d*g</td>
-                        <td>i</td>
-                        <td>j=d*i</td>
-                        <td>k=j*5% | j*15%</td>
+                        <td>{{ array_sum($totJumlahNR) }}</td>
+                        <td>{{ number_format( array_sum($totSatuanPnbpNr), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totJumlahPNBP), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totJasaTransport), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totJumTransport), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totJasPro), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totJumJasaProfesi), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totPPH), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totJumPengPPH), 2 ) }}</td>
+                        <td>{{ number_format( array_sum($totJumPerPem), 2 ) }}</td>
                     </tr>
                 </tbody>
             </table>
