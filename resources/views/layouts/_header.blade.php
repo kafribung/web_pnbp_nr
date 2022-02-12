a0c335fd144766ea052b7235f2b4d05cf0122558
kafribung <60900117001@uin-alauddin.ac.id>
2022-02-12 21:59:57 +0800

    header

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -18,7 +18,7 @@
         </div>
         <ul class="flex items-center flex-shrink-0 space-x-6">
             <!-- Theme toggler -->
-            <li class="flex">
+            {{-- <li class="flex">
                 <button class="rounded-md focus:outline-none focus:shadow-outline-green"
                 x-data="{
                     dark: false,
@@ -51,7 +51,7 @@
                         </svg>
                     </template>
                 </button>
-            </li>
+            </li> --}}
             <!-- Notifications menu -->
             <li x-data="{ toggleNotificationsMenu : false }" class="relative">
                 <button
