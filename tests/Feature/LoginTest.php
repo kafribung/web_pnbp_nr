f5833e54744384604a55359aca0a33439d9a6f4a
kafribung <60900117001@uin-alauddin.ac.id>
2022-02-27 14:53:46 +0800

    login test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,21 +2,36 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function halaman_login_dapat_dikunjungi()
+    {
+        $this->get('login')
+                ->assertStatus(200);
     }
+
+    /** @test */
+    public function sesorang_dapat_login()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $response = $this->post('login', [
+            'email'    => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+
+        $response->assertRedirect('dashboard');
+    }
+
 }
