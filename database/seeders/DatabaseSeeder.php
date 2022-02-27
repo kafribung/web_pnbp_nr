18619a9303154b2c4d6135ef92fa9d20b16f9ca1
kafribung <60900117001@uin-alauddin.ac.id>
2022-02-27 15:06:55 +0800

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory()->create();
+        \App\Models\User::factory()->create();
         $this->call([
             RoleSeeder::class,
             TypologySeeder::class,
